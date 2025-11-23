9ad1dc115210367155c474bdb995fc39ad341c8b
KuwarGautam-01 <kuwargautam01@gmail.com>
2025-11-23 13:33:22 +0530

    feat: add solution for Min & Max in Array (GFG)

---
+++ Arrays/MaxMinInArray.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+import java.util.ArrayList;
+
+public class MaxMinInArray {
+
+    public static ArrayList<Integer> getMinMax(int[] arr) {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num < min) min = num;
+            if (num > max) max = num;
+        }
+
+        result.add(min);
+        result.add(max);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 4, 3, -5, -4, 8, 6};
+
+        System.out.println(getMinMax(arr));  
+        // Output: [-5, 8]
+    }
+}
